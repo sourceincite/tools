565ecce4ae293fc3d3366e974012079ac08438ed
mr_me <steven@srcincite.io>
2016-11-18 16:04:48 -0600

    added nodbg

---
+++ nodbg.py
@@ -0,0 +1,15 @@
+from pykd import *
+import re
+
+"""
+A simple windbg plugin that uses pykd to patche the peb->BeingDebugged.
+"""
+
+peb = getProcessOffset()
+BeingDebugged = loadBytes(peb + 0x02, 0x1)[0]
+if (BeingDebugged == 1):
+	dprintln("(+) being debugged, patching...")
+	writeBytes(peb + 0x2, [0x00])
+	dprintln("(+) done!")
+else:
+	dprintln("(+) not being debugged")
